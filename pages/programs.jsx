1d2c0f26c65fd45da86ecb89919d72156af6d1bd
Amin78-code <54574832+Amin78-code@users.noreply.github.com>
2022-08-23 15:55:22 +0500

    error-removing

--- pages/programs.jsx
+++ pages/programs.jsx
@@ -8,9 +8,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Image from 'next/image'
-import Image1 from '../assets/images/Images_programs/Picture 9.jpg'
-import Image2 from '../assets/images/Images_programs/Picture 5.jpg'
-import Image3 from '../assets/images/Images_programs/Picture 7.jpg'
+import Image1 from '../assets/images/program/9.jpg'
+import Image2 from '../assets/images/program/5.jpg'
+import Image3 from '../assets/images/program/7.jpg'
 import {BsChevronLeft, BsChevronRight} from "react-icons/bs";
 
 export default function Programs() {
