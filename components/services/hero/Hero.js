b9dce9bc1d45c096265011a946144d8e32fb6b01
Amin78-code <54574832+Amin78-code@users.noreply.github.com>
2022-08-24 13:52:47 +0500

    services-update

--- components/services/hero/Hero.js
+++ components/services/hero/Hero.js
@@ -8,12 +8,12 @@ const Hero = () => {
   return (
     <section className="w-full pt-[40px] pb-[60px] md:py-[93px] bg-[#F2F2F2] relative overflow-hidden">
       <div className='absolute left-0 right-0 top-0 w-full h-[100%]'>
-        <Image src={pattern} className='' />
+        <div className='scale-[1.5]'><Image src={pattern} className='' /></div>
       </div>
       <div className='absolute left-0 right-0 top-0 w-full h-[100%] bg-[#f2f2f2] opacity-[.99]'>
       </div>
       <div className="myContainer flex flex-col justify-center relative z-[1]">
-          <h1 className="fpr text-[35px] md:text-[70px] 2xl:text-[100px] leading-[35px] md:leading-[70px] 2xl:leading-[100px] mx-auto text-[#0F2E3C] text-center max-w-[1374px] pt-[30px]">
+          <h1 className="fpr text-[35px] md:text-[75px] 2xl:text-[100px] leading-[35px] md:leading-[75px] 2xl:leading-[100px] mx-auto text-[#0F2E3C] text-center max-w-[1374px] pt-[30px]">
             Coaching services designed for your personal needs
           </h1>
           <p className="text-[12px] md:text-[24px] leading-[16px] md:leading-[26px] text-[#555555] text-center max-w-[1434px] mx-auto pt-[15px] md:pt-[55px]">
